1fd3237342acf9b7862d5cdbeaa1883b19fb7a0c
Andrey Sevruk <sevruk_a@ukr.net>
2016-08-13 19:27:10 +0300

    Rejected commit: Default

---
+++ GoIt/src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Andrew on 8/13/2016.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
